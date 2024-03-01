6cbac3bbe5f82dc1d9454aa5823565c27182492f
dyeboah4 <157760266+dyeboah4@users.noreply.github.com>
2024-02-29 19:51:17 -0500

    Update Lab 3 PSYCHSTATS.R

--- Lab 3 PSYCHSTATS.R
+++ Lab 3 PSYCHSTATS.R
@@ -24,3 +24,10 @@ p_tri_marital <- ggplot(data = brfss_tristate,
                                       color = MARSIMPLE,  fill = MARSIMPLE)) 
 
 p_tri_marital + geom_smooth() 
+For the lab I wanted to look at BMI and age and its connecion to marriage status. To make the graph easier to read I combined 'divorced' 'seperated' and 'widowed' into one catergory called 'DWS' 
+After running the data and creating a graph for it we can see that for all three groups (DWS, Married, and Never Married) their BMI increases at around age 40 and decreases at around age
+  60. We can predict that around that time people become more sedentary and with age it becomes harder to stay active so they put on some weigh. After 20 years however we can preduict that
+age starts to cause most people to lose weight. It is intresting though that the 'never married' population has the highest BMI numbers when compared to the other two groups. Is it possible that 
+'unmarried' people are more sedentary and live more unhealhty lifestyles than 'married' or 'DWS' individuals. It is also intresting that the 'married' group has the lowest BMI over time 
+one would predict that people settle more when they are married so dont mind if they put on a few pounds while 'never married' or 'DWS' people are actively looking for a partner so they 
+  try to stay as fit as possible to attract one. There is not enough data here to know for certain why these relationships exist but I will say that the data subverted my prediction. 
